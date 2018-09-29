4160cf1b166a80a19dd75656431683f03ddac1b6
Nikita Shelomov <nikshel@yandex-team.ru>
2018-09-29 10:42:03 +0500

    Вернул обычное решение

--- math.js
+++ math.js
@@ -2,8 +2,4 @@
 
 exports.isStar = true;
 
-
-// This function sum numbers.
-exports.sum = (...numbers) => numbers
-    .map(Number)
-    .reduce((a, b) => a + b, 0);
+exports.sum = (a, b) => Number(a) + Number(b);
